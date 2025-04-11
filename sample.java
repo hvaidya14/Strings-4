9c753979d3af8c7f31b9b75abdc36981cb2b6aca
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-04-11 14:30:38 -0700

    Update sample.java

--- sample.java
+++ sample.java
@@ -41,3 +41,32 @@ class Solution {
         }
     }
 }
+
+
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        
+        Arrays.sort(logs, (log1, log2) -> {
+            String[] strarray1 = log1.split(" ", 2);
+            String[] strarray2 = log2.split(" ", 2);
+            boolean isDigit1 = Character.isDigit(strarray1[1].charAt(0));
+            boolean isDigit2 = Character.isDigit(strarray2[1].charAt(0));
+            if (!isDigit1 && !isDigit2) {
+                int cmp = strarray1[1].compareTo(strarray2[1]);
+                if(cmp == 0) {
+                    return strarray1[0].compareTo(strarray2[0]);
+                } else {
+                    return cmp;
+                }
+            } else if (!isDigit1 && isDigit2) {
+                return -1;
+            } else if (isDigit1 && !isDigit2) {
+                return 1;
+            } else {
+                return 0;
+            }
+
+        });
+        return logs;
+    }
+}
